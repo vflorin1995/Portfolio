4ba901462705f2e122f907b0e4b0e4f804d7d56e
gandradep <eventsonido@gmail.com>
2022-09-05 15:24:57 +0100

    Finished mobile Menu functionalities

--- menu.js
+++ menu.js
@@ -1,8 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
+const xButton = Array.from(document.querySelectorAll('.menu li'));
 
+console.log(xButton);
 function open() {
     menu.classList.add('showMenu');
 }
 
+function close() {
+  menu.classList.remove('showMenu');
+}
+
 hamburger.addEventListener('click', open);
+
+xButton.forEach((item) => {
+  item.addEventListener('click',close);
+});
